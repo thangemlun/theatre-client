0a40c226525038e44fb83a86d81e4e7d208e047b
Thien <thien2357@gmail.com>
2024-06-04 21:50:55 +0700

    remove unused import

--- lib/HomePage/HomePage.dart
+++ lib/HomePage/HomePage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:http/http.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
